3a5a69dc70aa998967f89baddb3932d3724b45b4
moosongsong <songe08@gmail.com>
2021-04-10 19:26:25 +0900

    해시 문제 답안

---
+++ 프로그래머스_해시/해시__완주하지못한선수.java
@@ -0,0 +1,22 @@
+// 해시에 대한 부족한 공부를 채워야 할 것 같다.
+
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+
+        //정렬해서 비교하기
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        // 서로 다르면 바로 리턴하기
+        int i;
+        for (i = 0; i < completion.length; i++) {
+            if (!participant[i].equals(completion[i])){
+                return participant[i];
+            }
+        }
+
+        return participant[i];
+    }
+}
