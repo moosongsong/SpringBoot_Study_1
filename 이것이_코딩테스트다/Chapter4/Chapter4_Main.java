bfa09149daceebde016f83576dfa06a0a6b6d834
moosongsong <songe08@gmail.com>
2021-04-20 19:35:43 +0900

    chapter 4 디렉토리 생성

---
+++ 이것이_코딩테스트다/Chapter4/Chapter4_Main.java
@@ -0,0 +1,21 @@
+import greedy.Greedy_1;
+import greedy.Greedy_2;
+import greedy.Greedy_3;
+import greedy.Greedy_4;
+
+public class Chapter3_Main {
+    public static void main(String[] args) {
+
+//        Greedy_1 greedy_1 = new Greedy_1();
+//        System.out.println(greedy_1.solution(200));
+
+//        Greedy_2 greedy_2 = new Greedy_2();
+//        System.out.println(greedy_2.solution());
+
+//        Greedy_3 greedy_3 = new Greedy_3();
+//        System.out.println(greedy_3.solution());
+
+        Greedy_4 greedy_4 = new Greedy_4();
+        System.out.println(greedy_4.solution());
+    }
+}
