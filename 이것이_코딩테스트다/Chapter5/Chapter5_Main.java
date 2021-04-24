156f68fe5862909fd8972053aee43fcda98d1395
moosongsong <songe08@gmail.com>
2021-04-24 13:15:37 +0900

    Stack 사용해보기

--- 이것이_코딩테스트다/Chapter5/Chapter5_Main.java
+++ 이것이_코딩테스트다/Chapter5/Chapter5_Main.java
@@ -1,5 +1,13 @@
+import dfs_bfs.DFS1_Stack;
+import dfs_bfs.DFS2_Queue;
+
 public class Chapter5_Main {
     public static void main(String[] args) {
 
+//        DFS1_Stack stack = new DFS1_Stack();
+//        stack.solution();
+
+        DFS2_Queue queue = new DFS2_Queue();
+        queue.solution();
     }
 }
--- 이것이_코딩테스트다/Chapter5/dfs_bfs/DFS1_Stack.java
+++ 이것이_코딩테스트다/Chapter5/dfs_bfs/DFS1_Stack.java
@@ -1,7 +1,14 @@
 package dfs_bfs;
 
-public class DFS1_Stack {
-    public int solution(int N) {
+import java.util.Stack;
 
+public class DFS1_Stack {
+    public void solution() {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        Integer i = stack.pop();
+        System.out.println(i);
     }
 }
