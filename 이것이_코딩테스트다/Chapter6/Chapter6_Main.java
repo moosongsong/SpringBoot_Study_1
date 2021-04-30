c7e083cf0f6f510e306bc553c0fc3b2b97d98bcf
moosongsong <songe08@gmail.com>
2021-04-30 23:46:48 +0900

    선택정렬 예시 문제

--- 이것이_코딩테스트다/Chapter6/Chapter6_Main.java
+++ 이것이_코딩테스트다/Chapter6/Chapter6_Main.java
@@ -1,5 +1,9 @@
+import sorting.Selection_Sort;
+
 public class Chapter6_Main {
     public static void main(String[] args) {
-
+        Selection_Sort sort = new Selection_Sort();
+        sort.sort();
+        sort.printArray();
     }
 }
--- 이것이_코딩테스트다/Chapter6/sorting/Selection_Sort.java
+++ 이것이_코딩테스트다/Chapter6/sorting/Selection_Sort.java
@@ -1,5 +1,25 @@
 package sorting;
 
 public class Selection_Sort {
+    int[] array = new int[]{7, 5, 9, 0, 3, 1, 6, 2, 4, 8};
 
+    public void sort(){
+        for (int i = 0; i < array.length; i++) {
+            int min_index = i;
+            for (int j = i+1; j < array.length; j++) {
+                if(array[min_index] > array[j]) {
+                    min_index = j;
+                }
+            }
+            int temp = array[i];
+            array[i] = array[min_index];
+            array[min_index] = temp;
+        }
+    }
+
+    public void printArray(){
+        for  (int i:array){
+            System.out.println(i);
+        }
+    }
 }
