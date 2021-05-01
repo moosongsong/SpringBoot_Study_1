d40a59fb1f19307e125bb0f3d3ebcc7d565475da
moosongsong <songe08@gmail.com>
2021-05-01 16:53:36 +0900

    [fixed] README.md 삽입정렬, 퀵정렬 설명

---
+++ 이것이_코딩테스트다/Chapter6/sorting/Quick_Sort.java
@@ -0,0 +1,15 @@
+package sorting;
+
+public class Quick_Sort {
+    int[] array = new int[]{7, 5, 9, 0, 3, 1, 6, 2, 4, 8};
+
+    public void sort(){
+
+    }
+
+    public void printArray() {
+        for (int i : array) {
+            System.out.println(i);
+        }
+    }
+}
