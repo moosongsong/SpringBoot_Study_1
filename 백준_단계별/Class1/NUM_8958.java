1205f411a99b58a6de2fd2d3915c3e562ee33459
moosongsong <songe08@gmail.com>
2021-06-27 17:24:33 +0900

    [Upload] 백준 8958

---
+++ 백준_단계별/Class1/NUM_8958.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class NUM_8958 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int total = scanner.nextInt();
+        String [] questions = new String[total];
+        scanner.nextLine();
+        for (int i = 0; i < questions.length; i++) {
+            questions[i] = scanner.nextLine();
+        }
+
+        int max=0;
+        int [] score = new int[total];
+
+        for (int i = 0; i < questions.length; i++) {
+            max=0;
+            for (int j = 0; j < questions[i].length(); j++) {
+                if (questions[i].charAt(j)=='O'){
+                    max++;
+                    score[i]+=max;
+                }else{
+                    max=0;
+                }
+            }
+        }
+
+        for (int i = 0; i < score.length; i++) {
+            System.out.println(score[i]);
+        }
+    }
+}
