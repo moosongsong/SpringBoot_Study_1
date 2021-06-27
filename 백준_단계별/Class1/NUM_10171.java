0369ae7eb2388c647e214e1e30dea71efc62768f
moosongsong <songe08@gmail.com>
2021-06-27 17:24:50 +0900

    [Upload] 백준 10171

---
+++ 백준_단계별/Class1/NUM_10171.java
@@ -0,0 +1,8 @@
+public class NUM_10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|");
+    }
+}
