a97ea26f2d1161015269b0415952d0d9a5f3678c
moosongsong <songe08@gmail.com>
2021-07-01 17:05:59 +0900

    [Upload] 백준 20828 🌟 시간초과!

--- 백준_단계별/Class2/NUM_10828.java
+++ 백준_단계별/Class2/NUM_10828.java
@@ -1,5 +1,50 @@
+import java.util.Scanner;
+import java.util.Stack;
+
 public class NUM_10828 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Stack<Integer> stack = new Stack<>();
 
+        int numberOfLine = scanner.nextInt();
+        scanner.nextLine();
+
+        String[] lines = new String[numberOfLine];
+        for (int i = 0; i < lines.length; i++) {
+            lines[i] = scanner.nextLine().trim();
+        }
+
+        for (String line : lines) {
+            int temp;
+            if (line.equals("pop")) {
+                if (stack.empty()) {
+                    temp = -1;
+                } else {
+                    temp = stack.pop();
+                }
+            } else if (line.equals("size")) {
+                temp = stack.size();
+            } else if (line.equals("empty")) {
+                if (stack.empty()) {
+                    temp = 1;
+                } else {
+                    temp = 0;
+                }
+            } else if (line.equals("top")) {
+                if (stack.empty()){
+                    temp=-1;
+                }else {
+                    temp = stack.peek();
+                }
+            } else {
+                String[] ary = line.split(" ");
+                temp = Integer.parseInt(ary[1]);
+                stack.add(temp);
+                temp = -2;
+            }
+            if (temp !=-2){
+                System.out.println(temp);
+            }
+        }
     }
 }
