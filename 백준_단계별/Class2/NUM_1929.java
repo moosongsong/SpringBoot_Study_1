0aae4149b70989e65ad38e175a41eb91eb2b979e
moosongsong <songe08@gmail.com>
2021-07-13 09:35:20 +0900

    [Upload] 백준 1929 해결, 에라스토스테네스의 체 🌟

--- 백준_단계별/Class2/NUM_1929.java
+++ 백준_단계별/Class2/NUM_1929.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class NUM_1929 {
@@ -11,16 +10,18 @@ public class NUM_1929 {
         StringBuilder stringBuilder = new StringBuilder();
         int m = Integer.parseInt(stringTokenizer.nextToken());
         int n = Integer.parseInt(stringTokenizer.nextToken());
+        boolean[] prime = new boolean[n + 1];
+        prime[0] = prime[1] = true;
+
+        for (int j = 2; j <= Math.sqrt(prime.length); j++) {
+            if (prime[j]) continue;
+            for (int i = j * j; i < prime.length; i += j) {
+                prime[i] = true;
+            }
+        }
 
         for (int i = m; i <= n; i++) {
-            boolean isOdd = true;
-            for (int j = 2; j <= i / 2; j++) {
-                if (i % j == 0) {
-                    isOdd = false;
-                    break;
-                }
-            }
-            if (isOdd) {
+            if (!prime[i]) {
                 stringBuilder.append(i).append('\n');
             }
         }
