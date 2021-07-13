e97d3a606c8208e8b4c906e4d61da2c753366415
moosongsong <songe08@gmail.com>
2021-07-13 20:11:45 +0900

    [Upload] 백준 2108 😥 문제 이해 못함.....

--- 백준_단계별/Class2/NUM_2108.java
+++ 백준_단계별/Class2/NUM_2108.java
@@ -1,55 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class NUM_2108 {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder stringBuilder = new StringBuilder();
+
         int n = Integer.parseInt(bufferedReader.readLine().trim());
-        int[] ary = new int[n];
-        for (int i = 0; i < ary.length; i++) {
-            ary[i] = Integer.parseInt(bufferedReader.readLine().trim());
-        }
-        Arrays.sort(ary);
-        stringBuilder.append(average(ary)).append('\n');
-        stringBuilder.append(ary[ary.length / 2]).append('\n');
-        stringBuilder.append(many(ary)).append('\n');
-        stringBuilder.append(ary[ary.length - 1] - ary[0]).append('\n');
-        System.out.println(stringBuilder);
-    }
+        int[] ary = new int[8001];
 
-    public static long average(int[] ary) {
-        int result = 0;
-
-        for (int j : ary) {
-            result += j;
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        int median = 100000;
+        int mode = 100000;
+        for (int i = 0; i < n; i++) {
+            int temp = Integer.parseInt(bufferedReader.readLine().trim());
+            sum += temp;
+            ary[temp + 4000]++;
+            max = Math.max(max, temp);
+            min = Math.min(min, temp);
         }
 
-        return Math.round((double) result / (double) ary.length);
-    }
+        int countForMedian = 0;
+        int mode_max = 0;
+        boolean flag = false;
+        for (int i = min + 4000; i <= max + 4000; i++) {
+            if (ary[i] > 0) {
+                if (countForMedian < (n + 1) / 2) {
+                    countForMedian += ary[i];
+                    median = i - 4000;
+                }
 
-    public static int many(int[] ary) {
-        if (ary.length == 1) {
-            return ary[0];
-        }
-        int[] count = new int[10001];
-        int max = 0;
-        for (int i = 0; i < ary.length; i++) {
-            count[ary[i] + 5000] += 1;
-            max = Math.max(max, count[ary[i] + 5000]);
-        }
-        int result = 0;
-        for (int i = 0; i < count.length; i++) {
-            if (count[i] == max) {
-                result++;
-            }
-            if (result == 2) {
-                result = i - 5000;
-                break;
+                if (mode_max < ary[i]) {
+                    mode_max = ary[i];
+                    mode = i - 4000;
+                    flag = true;
+                } else if (mode_max == ary[i] && flag) {
+                    mode = i - 4000;
+                    flag = false;
+                }
             }
         }
-        return result;
+        System.out.println((int) Math.round((double) sum / n));    // 산술평균
+        System.out.println(median);    // 중앙값
+        System.out.println(mode);    // 최빈값
+        System.out.println(max - min);    // 범위
     }
 }
