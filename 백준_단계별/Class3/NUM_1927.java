5cf8a10b5bba5d357b8f6014ef89215924ab4ead
moosongsong <songe08@gmail.com>
2021-07-17 17:32:16 +0900

    [Upload] 백준 1927

--- 백준_단계별/Class3/NUM_1927.java
+++ 백준_단계별/Class3/NUM_1927.java
@@ -1,22 +1,22 @@
 import java.io.*;
 import java.util.PriorityQueue;
 
-public class NUM_11279 {
+public class NUM_1927 {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(bufferedReader.readLine());
-        PriorityQueue<Integer> heap = new PriorityQueue<>((o1, o2) -> (-Integer.compare(o1, o2)));
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
         for (int i = 0; i < n; i++) {
             int temp = Integer.parseInt(bufferedReader.readLine());
-            if (temp ==0){
-                if (heap.isEmpty()){
-                    bufferedWriter.write('0'+"\n");
-                }else{
-                    bufferedWriter.write(heap.poll() +"\n");
+            if (temp == 0) {
+                if (minHeap.isEmpty()) {
+                    bufferedWriter.write(0 + "\n");
+                } else {
+                    bufferedWriter.write(minHeap.poll() + "\n");
                 }
-            }else {
-                heap.add(temp);
+            } else {
+                minHeap.add(temp);
             }
         }
         bufferedWriter.flush();
