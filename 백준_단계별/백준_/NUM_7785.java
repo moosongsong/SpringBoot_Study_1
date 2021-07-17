2293c0888df01e6c21fe6e2a37a23d38e0e1c12b
moosongsong <songe08@gmail.com>
2021-07-18 00:10:33 +0900

    [Upload] 백준 7785

---
+++ 백준_단계별/백준_/NUM_7785.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+public class NUM_7785 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(bufferedReader.readLine());
+        StringTokenizer stringTokenizer;
+        TreeSet<String> employees = new TreeSet<>((o1, o2) -> -o1.compareTo(o2));
+        for (int i = 0; i < n; i++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
+            String name = stringTokenizer.nextToken();
+            String state = stringTokenizer.nextToken();
+            if (state.equals("enter")){
+                employees.add(name);
+            }else if(state.equals("leave")){
+                employees.remove(name);
+            }
+        }
+        for(String name:employees){
+            bufferedWriter.write(name+"\n");
+        }
+        bufferedWriter.flush();
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
