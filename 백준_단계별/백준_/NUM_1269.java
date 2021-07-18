d90e52e4bf06e42ff6d483793884747a331eb31d
moosongsong <songe08@gmail.com>
2021-07-18 16:26:16 +0900

    [Upload] 백준 1269

---
+++ 백준_단계별/백준_/NUM_1269.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+public class NUM_1269 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
+        int numOfA = Integer.parseInt(stringTokenizer.nextToken());
+        int numOfB = Integer.parseInt(stringTokenizer.nextToken());
+        TreeSet<Integer> setA = new TreeSet<>();
+        TreeSet<Integer> setB = new TreeSet<>();
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
+        for (int i = 0; i < numOfA; i++) {
+            setA.add(Integer.parseInt(stringTokenizer.nextToken()));
+        }
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
+        for (int i = 0; i < numOfB; i++) {
+            int temp = Integer.parseInt(stringTokenizer.nextToken());
+            if (setA.contains(temp)){
+                setA.remove(temp);
+            }else{
+                setB.add(temp);
+            }
+        }
+        bufferedWriter.write(String.valueOf(setA.size()+setB.size()));
+        bufferedWriter.flush();
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
