d2bdfdf83877b71cbe1a1d9d9942a49425caa238
moosongsong <songe08@gmail.com>
2021-07-19 15:14:59 +0900

    [Upload] 백준 5585

--- 백준_단계별/백준_/greedy/NUM_5585.java
+++ 백준_단계별/백준_/greedy/NUM_5585.java
@@ -1,28 +1,20 @@
 package greedy;
 
 import java.io.*;
-import java.util.HashSet;
-import java.util.StringTokenizer;
 
 public class NUM_5585 {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer stringTokenizer;
+        int[] remain = new int[]{500, 100, 50, 10, 5, 1};
+        int money = 1000 - Integer.parseInt(bufferedReader.readLine());
 
-        int num = Integer.parseInt(bufferedReader.readLine());
-        int line = Integer.parseInt(bufferedReader.readLine());
-        HashSet<Integer> [] people = new HashSet[line];
-        for (int i = 0; i < line; i++) {
-            stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
-            int count = Integer.parseInt(stringTokenizer.nextToken());
-            for (int j = 0; j < count; j++) {
-                int person = Integer.parseInt(stringTokenizer.nextToken());
-                people[i].add(person);
-            }
+        int count = 0;
+        for (int i = 0; i < remain.length; i++) {
+            count += money / remain[i];
+            money %= remain[i];
         }
-
-
+        bufferedWriter.write(String.valueOf(count));
 
         bufferedWriter.flush();
         bufferedReader.close();
