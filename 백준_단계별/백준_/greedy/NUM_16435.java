39efe8808067f56239a2fab8350041ac29bcd4e9
moosongsong <songe08@gmail.com>
2021-07-20 09:27:58 +0900

    [Upload] 백준 16435

---
+++ 백준_단계별/백준_/greedy/NUM_16435.java
@@ -0,0 +1,32 @@
+package greedy;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class NUM_16435 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
+        int n = Integer.parseInt(stringTokenizer.nextToken());
+        int l = Integer.parseInt(stringTokenizer.nextToken());
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
+        int[] apple = new int[n];
+        for (int i = 0; i < n; i++) {
+            apple[i] = Integer.parseInt(stringTokenizer.nextToken());
+        }
+        Arrays.sort(apple);
+        for (int i = 0; i < apple.length; i++) {
+            if (apple[i] > l) {
+                break;
+            } else {
+                l += 1;
+            }
+        }
+        bufferedWriter.write(String.valueOf(l));
+        bufferedWriter.flush();
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
